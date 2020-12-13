77f4e22f738f2cec8c528b7efe9d75173dc31b74
Richard <richard.yang@jingdigital.com>
2020-12-13 10:40:58 +0800

    updata

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-for="(item, index) in 16" :key="index" class="content"></div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home'
+}
+</script>
+<style lang="scss" scoped>
+.about{
+  height: calc(100vh - 100px);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  border: 1px solid red;
+}
+.content{
+  width: 100px;
+  height: 100px;
+  margin-bottom: 15px;
+  margin-right: 15px;
+  background: pink;
+  border-radius: 4px;
+}
+</style>
